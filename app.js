d3edf357bbb43a235f40705348861f6cea6b7d21
FingerPrintGames <kevinjordan199449@gmail.com>
2022-06-20 08:48:11 -0400

    Start work on the more advanced calculator app

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 `use strict`;
 
-const firstNumberField = document.querySelector(`.num1`);
-const secondNumberField = document.querySelector(`.num2`);
-const resultDisplay = document.querySelector(`.result`);
-const addBtn = document.querySelector(`.add`);
-const subtractBtn = document.querySelector(`.subtract`);
-const multiplyBtn = document.querySelector(`.multiply`);
-const divideBtn = document.querySelector(`.divide`);
+const numBtn = document.querySelectorAll(`.calc-btn`);
+const numDisplay = document.querySelector(`.num-display`);
 
 class Calculator {
-  constructor() {}
+  firstNum;
+  secondNum;
+
+  constructor() {
+    this.enterNumber();
+  }
 
   calculate(num1, num2, operation) {
     if (isNaN(num1)) num1 = 0;
     if (isNaN(num2)) num2 = 0;
     return operation(num1, num2);
+  }
+
+  // TODO: FIND OUT WHY THIS IS NOT WORKING
+  enterNumber() {
+    if (!this.firstNum) {
+      this.firstNum = toString(1);
+    } else {
+      this.firstNum += toString(1);
+    }
+    numDisplay.textContent = this.firstNum;
   }
 
   add(num1, num2) {
@@ -32,50 +42,6 @@ class Calculator {
   divide(num1, num2) {
     return num1 / num2;
   }
-
-  updateResult(result) {
-    resultDisplay.textContent = result;
-  }
 }
 
 const calculator = new Calculator();
-
-addBtn.addEventListener(`click`, function (e) {
-  e.preventDefault();
-  const result = calculator.calculate(
-    +firstNumberField.value,
-    +secondNumberField.value,
-    calculator.add
-  );
-  calculator.updateResult(result);
-});
-
-subtractBtn.addEventListener(`click`, function (e) {
-  e.preventDefault();
-  const result = calculator.calculate(
-    +firstNumberField.value,
-    +secondNumberField.value,
-    calculator.subtract
-  );
-  calculator.updateResult(result);
-});
-
-multiplyBtn.addEventListener(`click`, function (e) {
-  e.preventDefault();
-  const result = calculator.calculate(
-    +firstNumberField.value,
-    +secondNumberField.value,
-    calculator.multiply
-  );
-  calculator.updateResult(result);
-});
-
-divideBtn.addEventListener(`click`, function (e) {
-  e.preventDefault();
-  const result = calculator.calculate(
-    +firstNumberField.value,
-    +secondNumberField.value,
-    calculator.divide
-  );
-  calculator.updateResult(result);
-});
